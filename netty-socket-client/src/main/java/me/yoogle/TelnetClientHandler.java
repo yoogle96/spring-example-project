440a0fd9f8999a2d6b85578afa7dc79d48b56713
yoogle96 <dmltjs851@gmail.com>
2020-07-02 14:28:20 +0900

    Add TelnetClientHandler

--- netty-socket-client/src/main/java/me/yoogle/TelnetClientHandler.java
+++ netty-socket-client/src/main/java/me/yoogle/TelnetClientHandler.java
@@ -1,4 +1,18 @@
 package me.yoogle;
 
-public class TelnetClientHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class TelnetClientHandler extends SimpleChannelInboundHandler<String> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+        System.err.println(msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
 }
