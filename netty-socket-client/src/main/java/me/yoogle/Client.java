098c093aa3ae7d644307d77928be7e10ec673cdf
Yu UiSeon <dmltjs851@gmail.com>
2020-07-04 20:45:05 +0900

    Update socket client

--- netty-socket-client/src/main/java/me/yoogle/Client.java
+++ netty-socket-client/src/main/java/me/yoogle/Client.java
@@ -1,4 +1,39 @@
 package me.yoogle;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class Client {
+
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel sc) throws Exception {
+                            ChannelPipeline cp = sc.pipeline();
+                            cp.addLast(new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture cf = bootstrap.connect("localhost", 9999).sync();
+            cf.channel().closeFuture().sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
 }
--- netty-socket-client/src/main/java/me/yoogle/EchoClientHandler.java
+++ netty-socket-client/src/main/java/me/yoogle/EchoClientHandler.java
@@ -1,4 +1,40 @@
 package me.yoogle;
 
-public class EchoClientHandler {
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+public class EchoClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        String snedMessage = "Hello, Netty!";
+
+        ByteBuf messageBuffer = Unpooled.buffer();
+        messageBuffer.writeBytes(snedMessage.getBytes());
+        ctx.writeAndFlush(messageBuffer);
+
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+
 }
